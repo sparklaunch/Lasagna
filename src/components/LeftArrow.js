5d7c1d9a08a54af8183c2288bd921fad6d27e537
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-09 17:32:34 +0900

    [Add] Page flipping func.

--- src/components/LeftArrow.js
+++ src/components/LeftArrow.js
@@ -1,9 +1,16 @@
 import { AiOutlineLeft } from "react-icons/ai";
+import { useContext } from "react";
+import BookContext from "../contexts/BookContext";
 
 const LeftArrow = () => {
+  const { book } = useContext(BookContext);
+  const flipLeft = () => {
+    book.current.pageFlip().flipPrev();
+  };
   return (
     <div
       className={"fixed top-[50%] translate-y-[-50%] left-[5vw] cursor-pointer"}
+      onClick={flipLeft}
     >
       <AiOutlineLeft color={"white"} size={"3em"} />
     </div>
--- src/components/RIghtArrow.js
+++ src/components/RIghtArrow.js
@@ -1,11 +1,18 @@
 import { AiOutlineRight } from "react-icons/ai";
+import { useContext } from "react";
+import BookContext from "../contexts/BookContext";
 
 const RightArrow = () => {
+  const { book } = useContext(BookContext);
+  const flipRight = () => {
+    book.current.pageFlip().flipNext();
+  };
   return (
     <div
       className={
         "fixed top-[50%] translate-y-[-50%] right-[5vw] cursor-pointer"
       }
+      onClick={flipRight}
     >
       <AiOutlineRight color={"white"} size={"3em"} />
     </div>
