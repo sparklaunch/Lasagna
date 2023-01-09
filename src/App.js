937f5f27c7d91fcd0dfbf05ff99c54d115a71561
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-09 16:48:51 +0900

    [Update] position.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import Book from "./components/Book";
 
 const App = () => {
   return (
-    <div className={"bg-blue-300 h-[100vh]"}>
-      <Book />
+    <div className={"bg-blue-300 h-[100vh] overflow-clip"}>
+      <div className={"justify-center items-center flex h-full"}>
+        <Book />
+      </div>
     </div>
   );
 };
