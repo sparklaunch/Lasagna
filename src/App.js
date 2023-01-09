83a3f04efd2948b01d01adba239f8b06f27f41d5
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-09 17:25:33 +0900

    [Add] left arrow.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Book from "./components/Book";
 import { BookContextProvider } from "./contexts/BookContext";
+import LeftArrow from "./components/LeftArrow";
 
 const App = () => {
   return (
     <BookContextProvider>
       <div className={"bg-blue-300 h-[100vh] overflow-clip"}>
+        <LeftArrow />
         <div className={"justify-center items-center flex h-full"}>
           <Book />
         </div>
--- src/components/LeftArrow.js
+++ src/components/LeftArrow.js
@@ -1,5 +1,11 @@
+import { AiOutlineLeft } from "react-icons/ai";
+
 const LeftArrow = () => {
-  return <div></div>;
+  return (
+    <div className={"fixed top-[50%] translate-y-[-50%]"}>
+      <AiOutlineLeft color={"white"} size={"3em"} />
+    </div>
+  );
 };
 
 export default LeftArrow;
